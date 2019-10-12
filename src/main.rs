5d97db34f8e674e4c8e508b9a9a97a661e272572
Philip Peterson <carniojack424@gmail.com>
2019-10-12 02:33:14 -0700

    wip arg parsing

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ mod shapes;
 extern crate itertools;
 extern crate clap;
 
-use clap::{Arg, App};
+use std::iter::Iterator;
+use clap::{Arg, App, SubCommand, ArgGroup};
 use std::fs;
 use std::result::Result;
 
@@ -129,6 +130,48 @@ fn read_groups() -> Vec<GroupEntry> {
 }
 
 fn main() {
-    let users = read_users();
+    let matches = App::new("groupls")
+        .author("pc.peterso@gmail.com")
+        .version("1.0.0")
+        .about("explore group memberships")
+        .arg(
+            Arg::from_usage("--json")
+                .multiple(false)
+                .takes_value(false)
+                .required(false)
+                .help("If set, the output of this command will be a JSON blob")
+        )
+        .arg(
+            Arg::with_name("user_name")
+                .short("u")
+                .long("user")
+                .multiple(true)
+                .takes_value(true)
+                .required(false)
+                .help("A user, whose groups are to be listed out")
+        )
+        .arg(
+            Arg::with_name("group_name")
+                .short("g")
+                .long("group")
+                .multiple(true)
+                .takes_value(true)
+                .required(false)
+                .help("A group, whose users are to be listed out")
+        )
+        .after_help("Longer explaination to appear after the options when \
+            displaying the help information from --help or -h")
+        .get_matches();
+
+    let is_using_json = matches.is_present("json");
+    let groups_to_list = matches.values_of("group_name").map_or_else(|| Vec::new(), |z| z.collect::<Vec<_>>());
+    let users_to_list = matches.values_of("user_name").map_or_else(|| Vec::new(), |z| z.collect::<Vec<_>>());
+
     let groups = read_groups();
+
+    if (groups_to_list.len() == 0 && users_to_list.len() == 0) {
+        println!("There are 3 groups that exist");
+    }
+
+    let users = read_users();
 }
